6ea0f01dc1e5a3d76a87cac10b0fbc161aa10adf
Tushar Tanaji Kurade <tusharkurade1009@gmail.com>
2025-10-29 23:56:13 +0530

    Implement reverseVowels function in Solution class

---
+++ reverseVowels_shadowkakashi.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int l = 0, r = s.size() - 1;
+        string vowels = "aeiouAEIOU";
+        while (l < r) {
+            while (l < r && vowels.find(s[l]) == string::npos) l++;
+            while (l < r && vowels.find(s[r]) == string::npos) r--;
+            if (l < r) swap(s[l++], s[r--]);
+        }
+        return s;
+    }
+};
